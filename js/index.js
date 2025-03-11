2f8b3200ba9d3b8d07668881833b99f97ac5f2e9
Hemal Pandya <hemaliscoding@gmail.com>
2025-03-11 21:13:32 +0530

    Finishing

--- js/index.js
+++ js/index.js
@@ -4,22 +4,26 @@ $(document).ready(function(){
     var seconds = parseInt(timer.text().substring(0,2) * 60) + parseInt(timer.text().substring(3,5))
 
     function countdown(){
-        if(seconds > 0){
-        var curr_remaining_minutes = parseInt(seconds / 60)
-        var curr_remaining_seconds = seconds % 60
         
-        var formatted_minutes = String(curr_remaining_minutes).padStart(2,'0');
-        var formatted_seconds = String(curr_remaining_seconds).padStart(2,'0');
-
-        var curr_remaining_time = formatted_minutes + ":" + formatted_seconds
-
-        console.log(curr_remaining_time)        
-        timer.text(curr_remaining_time)
-
+        update_timer()
         seconds -= 1
         timeout = setTimeout(countdown,1000)
-        } else{
+        if(seconds < 0) {
             timer.text("00:00")
+        }
+    }
+
+    function update_timer(){
+        if(seconds > 0){
+            var curr_remaining_minutes = parseInt(seconds / 60)
+            var curr_remaining_seconds = seconds % 60
+            
+            var formatted_minutes = String(curr_remaining_minutes).padStart(2,'0');
+            var formatted_seconds = String(curr_remaining_seconds).padStart(2,'0');
+    
+            var curr_remaining_time = formatted_minutes + ":" + formatted_seconds
+           
+            timer.text(curr_remaining_time)
         }
     }
 
@@ -41,8 +45,8 @@ $(document).ready(function(){
         if(timeout){
             clearTimeout(timeout)
         }
-        seconds = 1500
-        timer.text("25:00")
+        $(".dialogue").removeClass("hidden")
+        $(".set-timer").removeClass("hidden")
         $(".play").removeClass("hidden")
         $(".pause").addClass("hidden")
     })
@@ -59,6 +63,14 @@ $(document).ready(function(){
         if(seconds.val() > 59){
             seconds.val(59)
         }
+    })
+    
+    $(".submit-button").click( () => {
+        seconds = parseInt($("#minutes").val() * 60) + parseInt($("#seconds").val())
+        console.log(seconds)
+        update_timer()
+        $(".dialogue").addClass("hidden")
+        $(".set-timer").addClass("hidden")
     })
 }
 );
